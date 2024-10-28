8c2a5ffe5a7b81809dd7c7cbd99f964025586cb1
Nazariy Holovach <nazariyholovach@Nazariys-MacBook-Air.local>
2024-10-28 15:18:29 +0200

    Add random background-color generator for notificationMessage

--- linkify_frontend/src/components/Notification.vue
+++ linkify_frontend/src/components/Notification.vue
@@ -1,77 +1,29 @@
 <template>
     <div class="w-full h-auto text-white border-y py-2">
-        <div class="w-full flex justify-between items-center bg-rose-500 my-2 py-2 px-4 rounded-2xl">
-            <div class="flex items-center">
-                <div class="bg-white p-2 rounded-2xl me-2">
-                    <div class="size-6 rounded-full loading"></div>
-                </div>
-                
-                <span><router-link to="empty">@Nazariy</router-link> started folowing you</span>
-            </div>
-            
-            <button class="text-xl">
-                <i class="fa-solid fa-circle-xmark"></i>
-            </button>
-        </div>
-        <div class="w-full flex justify-between items-center bg-purple-500 my-2 py-2 px-4 rounded-2xl">
-            <div class="flex items-center">
-                <div class="bg-white p-2 rounded-2xl me-2">
-                    <div class="size-6 rounded-full loading"></div>
-                </div>
-                
-                <span><router-link to="empty">@Nazariy</router-link> started folowing you</span>
-            </div>
-            
-            <button class="text-xl">
-                <i class="fa-solid fa-circle-xmark"></i>
-            </button>
-        </div>
-        <div class="w-full flex justify-between items-center bg-amber-500 my-2 py-2 px-4 rounded-2xl">
-            <div class="flex items-center">
-                <div class="bg-white p-2 rounded-2xl me-2">
-                    <div class="size-6 rounded-full loading"></div>
-                </div>
-                
-                <span><router-link to="empty">@Nazariy</router-link> started folowing you</span>
-            </div>
-            
-            <button class="text-xl">
-                <i class="fa-solid fa-circle-xmark"></i>
-            </button>
-        </div>
-        <div class="w-full flex justify-between items-center bg-pink-500 my-2 py-2 px-4 rounded-2xl">
-            <div class="flex items-center">
-                <div class="bg-white p-2 rounded-2xl me-2">
-                    <div class="size-6 rounded-full loading"></div>
-                </div>
-                
-                <span><router-link to="empty">@Nazariy</router-link> started folowing you</span>
-            </div>
-            
-            <button class="text-xl">
-                <i class="fa-solid fa-circle-xmark"></i>
-            </button>
-        </div>
-        <div class="w-full flex justify-between items-center bg-emerald-500 my-2 py-2 px-4 rounded-2xl">
-            <div class="flex items-center">
-                <div class="bg-white p-2 rounded-2xl me-2">
-                    <div class="size-6 rounded-full loading"></div>
-                </div>
-                
-                <span><router-link to="empty">@Nazariy</router-link> started folowing you</span>
-            </div>
-            
-            <button class="text-xl">
-                <i class="fa-solid fa-circle-xmark"></i>
-            </button>
-        </div>
+        <NotificationMessage />
+        <NotificationMessage />
+        <NotificationMessage />
+        <NotificationMessage />
+        <NotificationMessage />
+        <NotificationMessage />
+        <NotificationMessage />
+        <NotificationMessage />
+        <NotificationMessage />
+        <NotificationMessage />
+        <NotificationMessage />
     </div>
 </template>
 
 
 <script>
+import NotificationMessage from '@/components/NotificationMessage.vue'
+
+
 export default {
     name: 'Notification',
+    components: {
+        NotificationMessage,
+    },
     setup() {
 
     }
---
+++ linkify_frontend/src/components/NotificationMessage.vue
@@ -0,0 +1,33 @@
+<template>
+    <div :class="['w-full', 'flex', 'justify-between', 'items-center', 'my-2', 'py-2', 'px-4', 'rounded-2xl', randomColor]">
+        <div class="flex items-center">
+            <div class="bg-white p-2 rounded-2xl me-2">
+                <div class="size-6 rounded-full loading"></div>
+            </div>
+
+            <span><router-link to="empty">@Nazariy</router-link> started folowing you</span>
+        </div>
+
+        <button class="text-xl">
+            <i class="fa-solid fa-circle-xmark"></i>
+        </button>
+    </div>
+</template>
+
+
+<script>
+import { useColorsStore } from '@/stores/colors'
+import { ref } from 'vue'
+
+export default {
+    name: 'NotificationMessage',
+    setup() {
+        const colorsStore = useColorsStore()
+        const randomColor = ref(colorsStore.getRandomColor())
+
+        return {
+            randomColor
+        }
+    }
+}
+</script>
