fec7a349a0d39af0bde437ac6483aee22ca21a6b
Nazariy Holovach <nazariyholovach@Nazariys-MacBook-Air.local>
2024-10-29 09:44:53 +0200

    Added ability to remove notification

--- linkify_frontend/src/components/NotificationMessage.vue
+++ linkify_frontend/src/components/NotificationMessage.vue
@@ -1,5 +1,6 @@
 <template>
-    <div :class="['w-full', 'flex', 'justify-between', 'items-center', 'my-2', 'py-2', 'px-4', 'rounded-2xl', randomColor]">
+    <div :class="['w-full', 'flex', 'justify-between', 'items-center', 'my-2', 'py-2', 
+    'px-4', 'rounded-2xl', 'transition', 'ease-linear', 'duration-100', randomColor]">
         <div class="flex items-center">
             <div class="bg-white p-2 rounded-2xl me-2">
                 <div class="size-6 rounded-full loading"></div>
@@ -8,7 +9,7 @@
             <span><router-link to="empty">@Nazariy</router-link> started folowing you</span>
         </div>
 
-        <button class="text-xl">
+        <button @click="removeNotification($event)" class="text-xl">
             <i class="fa-solid fa-circle-xmark"></i>
         </button>
     </div>
@@ -25,8 +26,17 @@ export default {
         const colorsStore = useColorsStore()
         const randomColor = ref(colorsStore.getRandomColor())
 
+
+        function removeNotification(event) {
+            const notification = event.target.parentElement.parentElement
+            notification.classList.add('translate-x-full')
+
+            setTimeout(() => notification.remove(), 100)
+        }
+
         return {
-            randomColor
+            randomColor,
+            removeNotification
         }
     }
 }
