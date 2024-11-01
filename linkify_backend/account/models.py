126b62dd826619f9f7a3f8f7f3410f06439b2261
Nazariy Holovach <nazariyholovach@Nazariys-MacBook-Air.local>
2024-11-01 10:24:47 +0200

    Added default avatar

--- linkify_backend/account/models.py
+++ linkify_backend/account/models.py
@@ -37,7 +37,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=50, blank=True, null=True)
-    avatar = models.ImageField(upload_to='avatars', blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars', default='default_avatar.png', blank=True, null=True)
 
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
