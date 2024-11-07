672bd21b7165eb78c328a4feacad5dc1b979b769
Nazariy Holovach <nazariyholovach@Nazariys-MacBook-Air.local>
2024-11-07 13:41:49 +0200

    Created PostDetailView

--- linkify_frontend/src/router/index.js
+++ linkify_frontend/src/router/index.js
@@ -6,6 +6,7 @@ import StoriesView from '@/views/StoriesView.vue'
 import StoreView from '@/views/StoreView.vue'
 import ProfileView from '@/views/ProfileView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
+import PostDetailView from '@/views/PostDetailView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/post/:id/',
+      name: 'post-detail',
+      component: PostDetailView
     },
     {
       path: '/account/authentication/',
---
+++ linkify_frontend/src/views/PostDetailView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>Hello Worl</div>
+</template>
+
+
+<script>
+import { useRoute } from 'vue-router'
+export default {
+    name: 'PostDetailView',
+    setup() {
+        const route = useRoute()
+        const postId = route.params.id
+        console.log(postId)
+
+        return {}
+    }
+}
+</script>
